637e55c5626afd7b7a56a79279d1b33d0285baf8
unknown <sahilsiddiqui.dev@gmail.com>
2020-09-02 23:13:47 +0530

    commit

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -11,94 +11,57 @@ import AppsIcon from "@material-ui/icons/Apps";
 import { Button } from "@material-ui/core";
 import { connect } from "react-redux";
 import { authLogout } from "../../actions/auth";
-import { Container, Row, Col } from "react-bootstrap";
-import {} from "bootstrap/dist/css/bootstrap.min.css";
 
 class Menu extends React.Component {
   render() {
     let menu = (
       <div className="menu">
-        <Container fluid>
-          <Row>
-            <Col>
-              <div className="menu__info">
-                <Button className="dashboard-button">
-                  <HomeOutlinedIcon />
-                  <div>Dashboard</div>
-                  <ExpandMoreOutlinedIcon />
-                </Button>
-              </div>
-            </Col>
+        <Button className="dashboard-button">
+          <HomeOutlinedIcon />
+          <div>Dashboard</div>
+          <ExpandMoreOutlinedIcon />
+        </Button>
 
-            <Col>
-              <div className="menu__info">
-                <Button className="dashboard-button-2">
-                  <AppsIcon />
-                  <div>Apps</div>
-                  <ExpandMoreOutlinedIcon />
-                </Button>
-              </div>
-            </Col>
+        <Button className="dashboard-button-2">
+          <AppsIcon />
+          <div>Apps</div>
+          <ExpandMoreOutlinedIcon />
+        </Button>
 
-            <Col sm={1.2}>
-              <div className="menu__info">
-                <Button className="dashboard-button-2">
-                  <LayersOutlinedIcon />
-                  <div>UI Elements</div>
-                  <ExpandMoreOutlinedIcon />
-                </Button>
-              </div>
-            </Col>
+        <Button className="dashboard-button-2">
+          <LayersOutlinedIcon />
+          <div>UI Elements</div>
+          <ExpandMoreOutlinedIcon />
+        </Button>
 
-            <Col sm={1.2}>
-              <div className="menu__info">
-                <Button className="dashboard-button-2">
-                  <CreateOutlinedIcon />
-                  <div>Form & Tables</div>
-                  <ExpandMoreOutlinedIcon />
-                </Button>
-              </div>
-            </Col>
+        <Button className="dashboard-button-2">
+          <CreateOutlinedIcon />
+          <div>Form & Tables</div>
+          <ExpandMoreOutlinedIcon />
+        </Button>
+        <div className="logout menu__info">
+          <Button>
+            <FileCopyOutlinedIcon />
+            <div className="pages">Pages</div>
+            <ExpandMoreOutlinedIcon />
+          </Button>
+          <ul>
+            <li onClick={() => this.props.authLogout()}>
+              <Button>Logout</Button>
+            </li>
+          </ul>
+        </div>
+        <Button className="dashboard-button-2">
+          <EqualizerOutlinedIcon />
+          <div>Charts & Maps</div>
+          <ExpandMoreOutlinedIcon />
+        </Button>
 
-            <Col>
-              <div className="logout menu__info">
-                <Button>
-                  <FileCopyOutlinedIcon />
-                  <div className="pages">Pages</div>
-                  <ExpandMoreOutlinedIcon />
-                </Button>
-                <ul>
-                  <li onClick={() => this.props.authLogout()}>
-                    <Button>Logout</Button>
-                  </li>
-                </ul>
-              </div>
-            </Col>
-
-            <Col sm={1.2}>
-              <div className="menu__info">
-                <Button className="dashboard-button-2">
-                  <EqualizerOutlinedIcon />
-                  <div>Charts & Maps</div>
-                  <ExpandMoreOutlinedIcon />
-                </Button>
-              </div>
-            </Col>
-
-            <Col>
-              <div className="menu__info">
-                <Button className="dashboard-button-2">
-                  <RadioButtonUncheckedOutlinedIcon />
-                  <div>Others</div>
-                  <ExpandMoreOutlinedIcon />
-                </Button>
-              </div>
-            </Col>
-
-            <Col></Col>
-            <Col></Col>
-          </Row>
-        </Container>
+        <Button className="dashboard-button-2">
+          <RadioButtonUncheckedOutlinedIcon />
+          <div>Others</div>
+          <ExpandMoreOutlinedIcon />
+        </Button>
       </div>
     );
 
